12ee349901509858a95d50a002a4340d8465bbfc
Premathas A. <UT01609@unicomtic.com>
2024-10-26 15:12:32 +0530

    completed the taskmanager

---
+++ TaskManagerAPI/TaskManagerAPI/Data/TaskContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagerAPI.Models;
+
+namespace TaskManagerAPI.Data
+{
+    public class TaskContext : DbContext
+    {
+        public TaskContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<TaskItem> Tasks { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
